9a97dff6a431c0cec2bbc5beeb42e4179684c293
mscaudill <mscaudill@gmail.com>
2023-08-24 15:03:53 -0500

    [dev]: update to sandy's plotting to allow generic axes to be passed around to support multiple subplots and metric plot scripts to generate figures

---
+++ scripts/metric_plots.py
@@ -0,0 +1,50 @@
+import pickle
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from spectraprints.scripts.plotting import boxplot_dict
+
+# Set the file location and load
+FILENAME = 'stxbp1_mask_performances.pkl'
+BASEPATH = '/media/matt/Zeus/sandy/results/'
+
+fp = Path(BASEPATH).joinpath(FILENAME)
+with open(fp, 'rb') as infile:
+    performances = pickle.load(infile)
+
+# pop animals from performances dict
+animals = performances.pop('names')
+
+# create large figure and axarr
+fig, axarr = plt.subplots(2, 3, figsize=(13, 9))
+for idx, (metric_name, dic) in enumerate(performances.items()):
+
+    # compute row and column for this index
+    row, col = np.unravel_index(idx, shape=(2,3))
+    ax = boxplot_dict(dic, show_data=True, jitter=0.03, ax=axarr[row, col],
+                      showfliers=False, medianprops=dict(color='k'))
+
+    # manage ax props
+    ax.set_ylabel(metric_name, fontweight='bold')
+    ax.get_xaxis().set_ticks([])
+    ax.spines[['right', 'top']].set_visible(False)
+
+# create a legend
+colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red']
+axarr[-1,-2].legend(performances['accuracy'].keys(), labelcolor=colors,
+        handlelength=0, handletextpad=0)
+
+# remove last axis
+axarr[-1,-1].axis('off')
+
+# manage plt properties
+plt.suptitle(f'STXBP1 Thresholding with Merge Radius = 0.5 secs'
+             f'\n n = {len(animals)} animals')
+plt.subplots_adjust(wspace=.4, hspace=.2)
+plt.show()
+
+
+
+
--- scripts/plotting.py
+++ scripts/plotting.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 def boxplot_dict(data: Dict, show_data: bool = False, jitter: float = 0,
-                 **kwargs):
+                 ax=None, **kwargs):
     """Boxplots the data in dictionary using keys as labels.
 
     Args:
@@ -23,24 +23,13 @@ def boxplot_dict(data: Dict, show_data: bool = False, jitter: float = 0,
         A matplotlib axes instance.
     """
     
-    fig, ax = plt.subplots()
+    if ax is None:
+        fig, ax = plt.subplots()
     
     groups = data.keys()
     ax.boxplot(data.values(), labels=groups, **kwargs)
     if show_data:
         for idx, values in enumerate(data.values(), 1):
             locs = np.random.uniform(-jitter, jitter, len(values)) + idx
-            plt.scatter(locs, values)
+            ax.scatter(locs, values)
     return ax
-
-if __name__ == '__main__':
-
-    data = {'sleep': 3*np.random.random(10), 'awake': 5*np.random.random(22)}
-    
-    ax = boxplot_dict(data, show_data=True, jitter=0.05)
-
-    ax.set_ylabel('counts')
-    ax.set_xlabel('state')
-    ax.set_title('some weird data')
-    plt.show()
-
