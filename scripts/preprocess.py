134e4f1232a3c5df14d5efe1d77d429d50b1de53
mscaudill <mscaudill@gmail.com>
2023-08-26 13:47:57 -0500

    [dev]: batch preprocessing ready to go but should check written files are compatible with Spindle software

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -23,13 +23,17 @@ from spectraprints.masking import masks
 
 
 def preprocess(epath, savedir, channels=[0,1,2], fs=5000, M=20, fstop=60,
-               stop_width=6, nominally=[48, 72], chunksize=30e5, axis=-1):
+               fwidth=6, nominally=[48, 72], chunksize=30e5, axis=-1,
+               returning=False, verbose=False):
     """Preprocesses and EDF file at path by constructing, notch filtering and
     downsampling a producer.
 
     Args:
         epath:
             Path to an edf file.
+        savedir:
+            Directory location where preprocessed EDF will be saved to. The
+            filename to save will be named epath.stem + '_preprocessed'. 
         channels:
             The channels to include in the processed producer.
         fs:
@@ -37,104 +41,141 @@ def preprocess(epath, savedir, channels=[0,1,2], fs=5000, M=20, fstop=60,
         M:
             The downsample factor to reduce the number of samples produced by
             the returned processed producer.
-        start:
-            The start index at which production begins. If None start is 0.
-        stop:
-            The stop index at which production stops. If None stop is data
-            length along axis.
+        fstop:
+            The stop frequency of the preprocessing Notch IIR filter.
+        fwidth:
+            The width of the preprocessing Notch IIR filter.
+        nominally:
+            A sequence of expected lengths. The preprocessed data will be
+            trimmed to match the nearest floor of this array. Example if epath
+            data is 66 hours long it will be trimmed to 48 hours.
         chunksize:
             The number of processed samples along axis to yield from the 
-            producer at a time.
+            producer at a time. When called in a multiprocessing context this
+            value should be set to the amount of samples any single process uses
+            at one time.
         axis:
             The axis of reading and production.
+        returning:
+            Boolean indicating if preprocessed results should be returned.
+        verbose:
+            Boolean indicating if timing information should be printed to
+            stdout.
 
     Returns:
-        A producer of preprocessed values.
+        None if not returning else returns preprocessed data array.
     """
 
-    # TODO remove
     t0 = time.perf_counter()
 
     epath = Path(epath)
     reader = edf.Reader(epath)
     reader.channels = channels
 
-    # compute stop index to trim the readers data to one of nominally
-    # FIXME what if 68 hours -> we should use 48 not 72 
-    n = np.array(nominally) * 3600 * fs
-    stop = n[np.argmin(np.abs(n - reader.shape[axis]))]
-    if np.all(stop < nominally):
-        name = epath.stem[0:20] + '...'
-        msg = (f'The duration of file {name} is {reader.shape / (3600*fs)}' 
-               f' hours which is < expected = {nominally}')
-        warnings.warn(msg)
+    # Trim data with floor chosen from nominals
+    nsamples = np.array(nominally) * 3600 * fs
+    idx = np.searchsorted(nsamples, reader.shape[axis], side='right') - 1
+    if idx < 0:
+        hrs = reader.shape[axis] / (3600 * fs)
+        msg = (f'File {epath.stem} is {hrs} hrs, lower than all expected'
+               f' durations {nominally} hrs.')
+        raise ValueError(msg)
+    stop = nsamples[idx]
+
+    # ensure start to stop is evenly divisible by samples per record = fs/M
+    # if stop ~ k * fs then stop/M ~ k * fs / M where k is integer
+    stop = int(np.ceil(stop / fs) * fs)
 
     # build producer
     pro = masks.between_pro(reader, 0, stop, chunksize, axis)
 
-    # Notch filter the producer
-    notch = iir.Notch(fstop, width=stop_width, fs=fs)
+    # Notch filter and downsample the producer
+    notch = iir.Notch(fstop, width=fwidth, fs=fs)
     result = notch(pro, chunksize, axis, dephase=False)
-
-    # downsample the producer
     result = resampling.downsample(result, M, fs, chunksize, axis)
     
-    # in-memory compute and write
-    # TODO this will change once openseize accepts producers to write from
+    # in-memory compute & write, FIXME allow writers to write pros
     x = result.to_array()
 
-    print(x.shape)
+    # update headers samples_per_record & num_records fields
+    header = reader.header.filter(channels)
+    header['samples_per_record'] = [spr // M 
+            for spr in reader.header['samples_per_record']]
+    header['num_records'] = int(x.shape[axis] / (fs / M))
 
-    # create a header with new samples_per_record
-    new_header = reader.header.filter(channels)
-    new_header['samples_per_record'] = [fs // M for _ in channels]
-    
-    # FIXME we've trimmed samples to 48 or 72 hours so num_records is trimmed
-    # but we still need to ensure whole number of records. This may require
-    # padding x or trimming to the nearest number of records rather than exact
-    # hours
-    new_header['num_records'] = int(x.shape[axis] / (fs/M))
-
-    print(new_header)
-    
-    fname = epath.stem + '_downsampled' + epath.suffix
+    # write the file
+    fname = epath.stem + '_preprocessed' + epath.suffix
     filepath = Path(savedir).joinpath(fname)
     with edf.Writer(filepath) as writer:
-        writer.write(new_header, x, channels=channels)
+        writer.write(header, x, channels=channels, verbose=False)
 
-    print(f'Preprocessing finished in {time.perf_counter() - t0} secs')
-    reader.close()
-    return x
-
-
-def batch(dirpath, savedir, ncores=None, **kwargs):
-    """ """
+    if verbose:
+        elapsed = time.perf_counter() - t0
+        print(f'Preprocessing & file writing finished in {elapsed} secs')
     
-    t0 = time.perf_counter()
+    # clean up reader resources & possibly return
+    reader.close()
+    if returning:
+        return x
 
+
+def batch(dirpath, savedir, ncores=None, verbose=True, **kwargs):
+    """Preprocesses all EDF files in dir path saving the preprocessed data to
+    savedir. 
+
+    Args:
+        dirpath:
+            A directory containing EDF files to preprocess.
+        savedir:
+            A directory to where preprocessed data will be save in EDF format.
+        ncores:
+            The number of processing cores to concurrently preprocess EDF files
+            in dirpath.
+        **kwargs:
+            Any valid kwarg for the preprocess function of this module.
+
+    Returns:
+        None
+    """
+    
     epaths = list(Path(dirpath).glob('*edf'))
     
-    func = partial(preprocess, **kwargs)
+    t0 = time.perf_counter()
+    if verbose:
+        msg = (f'Executing Batch Preprocessor for {len(epaths)} files'
+               f' in {dirpath}')
+        print(msg)
+    
+    # returns are not allowed in batch processing
+    kwargs.pop('returning', False)
+    
+    func = partial(preprocess, savedir=savedir, **kwargs)
     workers = concurrency.set_cores(ncores, len(epaths))
     with Pool(workers) as pool:
-        pool.map(f, epaths)
+        pool.map(func, epaths)
 
     elapsed = time.perf_counter() - t0
     msg = f'Saved {len(epaths)} files to {savedir} in {elapsed} s'
     print(msg)
 
+
+
 if __name__ == '__main__':
 
-    basepath  = '/media/matt/Zeus/jasmine/stxbp1/'
-    name = 'CW0DI2_P097_KO_92_30_3dayEEG_2020-05-07_09_54_11.edf'
-
+    basepath = '/media/matt/Zeus/jasmine/test/'
+    
+    """
+    name = 'CW0DI3_P097_KO_93_31_3dayEEG_2020-05-07_09_54_14.edf'
     epath = Path(basepath).joinpath(name)
 
-    x = preprocess(epath, savedir='/media/matt/Zeus/sandy/test/')
+    x = preprocess(epath, savedir='/media/matt/Zeus/sandy/test/', verbose=True)
 
-    fp = ('/media/matt/Zeus/sandy/test/'
-         'CW0DI2_P097_KO_92_30_3dayEEG_2020-05-07_09_54_11_downsampled.edf')
-    reader = edf.Reader(fp)
+    savepath = '/media/matt/Zeus/sandy/test/' + '_processed' + '.edf'
+    reader = edf.Reader(savepath)
     y = reader.read(start=0)
 
     print(np.allclose(x, y, atol=0.5))
+    """
+
+    savedir = '/media/matt/Zeus/sandy/test/'
+    batch(basepath, savedir)
